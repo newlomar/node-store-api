00c62e81033e8b41faf2b2efc76d652c52422563
Newton Lomar <newtonlomar16@gmail.com>
2022-12-04 13:29:16 -0300

    filtering the query object for only use the possible information that our products can have, in this case, only for 'featured'

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,15 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const products = await Product.find(req.query)
+    const { featured } = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
     res.status(200).json({ products, amount: products.length })
 }
 
