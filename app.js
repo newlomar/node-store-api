a0751999762d7248e1d908c8b95b83db00c2f8d3
Newton Lomar <newtonlomar16@gmail.com>
2022-12-04 12:09:59 -0300

    adding routes and controllers

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/not-found')
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
 
+app.use('/api/v1/products', productsRouter)
 
 // products route
 
