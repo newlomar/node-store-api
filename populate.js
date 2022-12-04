f572e94e36dcb5af79f7e33c539a7450732d6cd6
Newton Lomar <newtonlomar16@gmail.com>
2022-12-04 12:40:46 -0300

    adding success message to populate DB file

--- populate.js
+++ populate.js
@@ -9,6 +9,7 @@ const jsonProducts = require('./products.json')
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
+        console.log('success')
     } catch (error) {
         console.log(error)
     }
