93ee21a7212bcdb38230ea092f08c2b3cc2a052b
Newton Lomar <newtonlomar16@gmail.com>
2022-12-04 12:49:50 -0300

    adding process.exit to populate file

--- populate.js
+++ populate.js
@@ -12,8 +12,10 @@ const start = async () => {
         await Product.deleteMany();
         await Product.create(jsonProducts)
         console.log('success')
+        process.exit(0)
     } catch (error) {
         console.log(error)
+        process.exit(1)
     }
 }
 
