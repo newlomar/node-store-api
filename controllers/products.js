dc7caef33c826d02db91b9d752ea7721e608241c
Newton Lomar <newtonlomar16@gmail.com>
2022-12-04 13:19:07 -0300

    filtering with query params

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,13 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({})
-    res.status(200).json({ products })
+    const products = await Product.find({name: 'vase table'})
+    res.status(200).json({ products, amount: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products route'})
+    const products = await Product.find(req.query)
+    res.status(200).json({ products, amount: products.length })
 }
 
 module.exports = {
