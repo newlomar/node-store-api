d6c332ab341ab6e7233cd9a254679899a170798f
Newton Lomar <newtonlomar16@gmail.com>
2022-12-04 14:44:27 -0300

    adding sort, from Z to A and by smallest price first

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({name: 'vase table'})
+    const products = await Product.find({}).sort('-name price')
     res.status(200).json({ products, amount: products.length })
 }
 
