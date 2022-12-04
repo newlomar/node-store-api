1a7a62976cf8964a6b6c05c32603abccfd16b82a
Newton Lomar <newtonlomar16@gmail.com>
2022-12-04 12:43:03 -0300

    adding cleanup and populate with json functions to DB

--- populate.js
+++ populate.js
@@ -9,6 +9,8 @@ const jsonProducts = require('./products.json')
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
+        await Product.deleteMany();
+        await Product.create(jsonProducts)
         console.log('success')
     } catch (error) {
         console.log(error)
