fc1b2cae80853fa595b192f5326629a085c1c4ad
Newton Lomar <newtonlomar16@gmail.com>
2022-12-04 16:34:05 -0300

    adding limit and pagination with skip functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).select('name price')
+    const products = await Product.find({}).select('name price').limit(33)
     res.status(200).json({ products, amount: products.length })
 }
 
@@ -34,6 +34,12 @@ const getAllProducts = async (req, res) => {
         const fieldsList = fields.split(',').join(' ');
         result = result.select(fieldsList)
     }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit)
 
     const products = await result
     res.status(200).json({ products, amount: products.length })
