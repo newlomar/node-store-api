0bf372ae9214d808566928e6f9a99d332e64153c
Newton Lomar <newtonlomar16@gmail.com>
2022-12-04 17:36:59 -0300

    adding numeric field search

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort, fields } = req.query
+    const { featured, company, name, sort, fields, numericFields } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -21,6 +21,35 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: 'i' }
     }
 
+    if (numericFields) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte'
+        }
+
+        const regEx = /\b(<|>|>=|=|<=)\b/g;
+
+        let filters = numericFields.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`
+        )
+        
+        const options = ['price', 'rating'];
+
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] =  item.split('-')
+
+            if(options.includes(field)) {
+                queryObject[field] = {[operator]: Number(value)}
+            }
+        })
+
+    }   
+    
+    console.log(queryObject)
     let result = Product.find(queryObject)  
 
     if (sort) {
