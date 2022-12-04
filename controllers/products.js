0df1cc8c0bc2be142619791eb26d6f93f0333f1f
Newton Lomar <newtonlomar16@gmail.com>
2022-12-04 15:54:54 -0300

    adding sort functionality, working with mult parameters

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -21,7 +21,16 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: 'i' }
     }
 
-    const products = await Product.find(queryObject)
+    let result = Product.find(queryObject)  
+
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+
+    const products = await result
     res.status(200).json({ products, amount: products.length })
 }
 
